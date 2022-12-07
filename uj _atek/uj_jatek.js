572ea7b065f8047133aa6079f6cb26aded8c6be5
Ordog-David <112890054+Ordog-David@users.noreply.github.com>
2022-12-07 11:56:25 +0100

    Correct one

--- uj _atek/uj_jatek.js
+++ uj _atek/uj_jatek.js
@@ -1,36 +1,51 @@
 let n = 5;
-let cordinates = [];
+var ranumber = [];
 let i = 0;
 let lista = [];
+let num = -1
+let k = 0
 
+function generalas(n) {
+    while(ranumber.length < n*n) {
+    var num = Math.round(Math.random() * (n*n-1));
+        if(ranumber.indexOf(num) === -1) {
+            ranumber.push(num);
+        }     
+    }
+helyezes(n)}
+generalas(n);
+console.log(ranumber)
 
- 
-function generalas (target) {
+function helyezes(n) {
+    for (let i = 0; i < n; i++) {
+        for (let j = 0; j < n; j++) {
+            console.log(j,lista)
+            lista.push[i,j,ranumber[k]]
+            k++ 
+        }
+    }
+} 
+function nezes (target) {
+    x = target.cellIndex
+    y = target.parentElement.rowIndex
     if (target.classList.contains('felfedve')){
         alert("Ne csalj!")
     }
     else {
-        let num = Math.floor(Math.random() * (n*n))
-        while (cordinates.includes(num)) {
-            let num = Math.floor(Math.random() * (n*n))
-            console.log(num)
-        }
-        console.log("Siker")
+        console.log("B",target)
         target.setAttribute('class', 'felfedve') 
         target.innerHTML = num 
-        cordinates.push(num);
-               
     }
 }
 
+
 (init = () => {
+    table = Array(n).fill().map(() => Array(n).fill())
     document.getElementById('t').innerHTML = `
     <table>
         ${Array(n).fill(`
         <tr>
-        ${Array(n).fill(`
-        <td onmouseup="onClick(this)" 
-        generalas(this)"/>.join('')}
+        ${Array(n).fill(`<td onmouseup="nezes(this)" />`).join('')}
         </tr>
         `).join('')}
     </table>`
