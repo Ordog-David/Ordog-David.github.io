45cad678e8524d55d1120860de68e2c3243a8bab
Ordog-David <112890054+Ordog-David@users.noreply.github.com>
2023-03-29 11:53:40 +0200

    Karomkodas cenzúra

--- billentyuzet2/kezelo_2.js
+++ billentyuzet2/kezelo_2.js
@@ -1,4 +1,9 @@
+let popszakma = "nem ilyen"
+let betuk_lista = []
+let convert = ""
+let beirt = document.getElementById('szoveg').innerHTML
 function billentyuzet(id, billentyuk) {
+  
   let table = document.createElement('table');
   table.className = "billentyuzet"
   billentyuk.forEach(function(rowData) {
@@ -6,7 +11,7 @@ function billentyuzet(id, billentyuk) {
     rowData.forEach(function(cellData) {
       let cell = row.insertCell();
       cell.billentyu = cellData.billentyu;
-
+      
       if ("szoveg" in cellData) {
         cell.textContent = cellData.szoveg;
       } else {
@@ -20,14 +25,54 @@ function billentyuzet(id, billentyuk) {
       if ("rowSpan" in cellData) {
         cell.rowSpan = cellData.rowSpan;
       }
-
       cell.addEventListener("click", (event) => onClick(event.target));
     });
   });
   document.getElementById(id).appendChild(table);
 }
 function onClick(target) {
-  document.getElementById('szoveg').innerHTML += target.billentyu;
+  betuk_lista.push(target.billentyu)
+  console.log(betuk_lista)
+  if (target.billentyu === "ㅤ") {
+    console.log("siker")
+    betuk_lista.pop()
+    betuk_lista.pop()
+    console.log("l:",betuk_lista)
+    convert = betuk_lista.toString()
+    console.log("b:",convert)
+    convert = convert.replaceAll(",","")
+    beirt = convert
+  }
+  else {
+    beirt += target.billentyu
+  }
+  document.getElementById('szoveg').innerHTML = beirt;
+  /*Szukseg van:
+    - beolvasni a karomkodas listat
+    - megnezni van-e a szovegben karomkodas
+    - megtalalni melyik karomkodast tartalmazza
+    - megtalalni a karomkodas helyet a szovegben
+    - valasz a karomkodasra
+    - backspace-t megcsinalni
+  if (beirt.includes("K")) {
+    console.log(beirt.indexOf())
+
+  } */
+  if (beirt.includes("ILYEN EZ A POPSZAKM") && popszakma == "nem ilyen") {
+    alert("Ne tedd")
+    popszakma = "ilyen"
+  }
+  for (var i = 0; i < karmokodas.length; i++) {
+    if (beirt.includes(karmokodas[i])) {
+      alert("Csúnyaszó észlelve: "+ karmokodas[i])
+      alert("Cenzúra indítása")
+      setTimeout(crasher, 3000)
+      
+      
+    }
+  }
+  
+  console.log(typeof beirt)   
 }
 let foBillentyuk = [
   [
@@ -82,9 +127,54 @@ let numerikusBillentyuk = [
     { billentyu: "9" }
   ],
   [
-    { billentyu: ",", colSpan: 2 },
-    { billentyu: "0" }
+    { billentyu: "," },
+    { billentyu: "0" },
+    //{ billentyu: "C" },
+    { billentyu: "ㅤ" , szoveg:"←"} //←
   ]
 ];
+//beirt = beirt.substring(0, beirt.lenght-2)
+//let karomkodasok = FileReader.readAsArrayBuffer(karomkodas.txt)
+/* ‎  */
+//ㅤ ㅤ 
+
+function crasher() {
+  for (var j = 0; j < 4; j++) {
+    visszaszamlalas = 3-j
+    alert(visszaszamlalas)
+  }
+  console.log("Cenzúra elkezdése")
+  for (var i = 5; i > 3; i = i + 1)
+    {console.log(i); }
+}
+
 billentyuzet("billentyuzet", foBillentyuk);
 billentyuzet("billentyuzet", numerikusBillentyuk);
+
+let karmokodas = [
+  "ILYEN EZ A POPSZAKMA",
+  "SUS",
+  "AMOGUS",
+  "FASZ",
+  "SZAR",
+  "GECI",
+  "PICSA",
+  "KURVA",
+  "POCS",
+  "BASZ",
+  "KURT",
+  "BAZD",
+  "AMONGUS",
+  "KRETEN",
+  "NIGG",
+  "FUTYI",
+  "CIGAN",
+  "SPERMA",
+  "SEGG",
+  "CSOCS",
+  "LESZBIKUS",
+  "MASZTURBACIO",
+  "SZOP",
+  "PENISZ",
+  "PINA",
+]
